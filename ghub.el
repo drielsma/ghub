195aaa6517162dbce0da74762296ab990793655d
Jonas Bernoulli <jonas@bernoul.li>
2017-08-09 16:19:00 +0200

    Remove most documentation before refactoring

--- ghub.el
+++ ghub.el
@@ -28,56 +28,6 @@
 ;; This library just provides the HTTP methods.
 ;; See https://developer.github.com/v3 for valid requests.
 
-;; Initial configuration
-;; ---------------------
-;;
-;;   $ git config --global github.user <username>
-;;   $ emacs ~/.authinfo.gpg
-;;   # -*- epa-file-encrypt-to: ("A.U.Thor@example.com") -*-
-;;   machine api.github.com login <login> password <token>
-;;
-;; To acquire a token, go to https://github.com/settings/tokens.  Note
-;; that currently the same token is shared by all Emacs packages that
-;; use `ghub.el'.
-
-;; Usage examples
-;; --------------
-;;
-;; Getting details about a repository:
-;;
-;;   (ghub-get "/repos/tarsius/ghub")
-;;
-;; Listing names of all repositories of a user:
-;;
-;;   (--keep (cdr (assq 'name it))
-;;           (let ((ghub-unpaginate t))
-;;             (ghub-get "/users/tarsius/repos")))
-;;
-;; Making an unauthenticated request:
-;;
-;;   (let ((ghub-authenticate nil))
-;;     (ghub-get "/orgs/magit/repos"))
-;;
-;; Making a request using basic authentication:
-;;
-;;   (let ((ghub-authenticate 'basic))
-;;     (ghub-get "/orgs/magit/repos"))
-
-;; Github Enterprise support
-;; -------------------------
-;;
-;; Initial configuration:
-;;
-;;   $ git config --global github.gh.example.com.user employee
-;;   $ emacs ~/.authinfo.gpg
-;;   # -*- epa-file-encrypt-to: ("employee@example.com") -*-
-;;   machine gh.example.com login employee password <token>
-;;
-;; Making a request:
-;;
-;;   (let ((ghub-base-url "https://gh.example.com"))
-;;     (ghub-get "/users/employee/repos"))
-
 ;;; Code:
 
 (require 'auth-source)
@@ -151,11 +101,7 @@ optional NOERROR is non-nil, in which case return nil."
 (define-error 'ghub-422 "Unprocessable Entity" 'ghub-http-error)
 
 (defun ghub-request (method resource &optional params data noerror)
-  "Make a request using METHOD for RESOURCE.
-METHOD is a `HTTP' request method, a string.  If non-nil, send
-PARAMS and/or DATA in the request.  Signal an error if the status
-code isn't in the 2xx class; unless optional NOERROR is non-nil,
-in which case return nil."
+  "Make a request for RESOURCE using METHOD."
   (let* ((p (and params (concat "?" (ghub--url-encode-params params))))
          (d (and data   (encode-coding-string (json-encode-list data) 'utf-8)))
          (url-request-extra-headers

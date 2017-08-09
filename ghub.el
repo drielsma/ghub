f56cd31c34d420fa50a61d72f442298d3a342bf3
Jonas Bernoulli <jonas@bernoul.li>
2017-08-09 16:22:00 +0200

    ghub--hostname: move definition

--- ghub.el
+++ ghub.el
@@ -207,12 +207,6 @@ optional NOERROR is non-nil, in which case return nil."
           (ghub--username))
     (url-basic-auth url t)))
 
-(defun ghub--hostname ()
-  (save-match-data
-    (if (string-match "\\`https?://\\([^/]+\\)" ghub-base-url)
-        (match-string 1 ghub-base-url)
-      (signal 'ghub-auth-error '("Invalid value for ghub-base-url")))))
-
 (defun ghub--token ()
   (or ghub-token
       (let ((secret (plist-get (car (auth-source-search
@@ -224,6 +218,12 @@ optional NOERROR is non-nil, in which case return nil."
                 (funcall secret)
               secret)
             (signal 'ghub-auth-error '("Token not found"))))))
+
+(defun ghub--hostname ()
+  (save-match-data
+    (if (string-match "\\`https?://\\([^/]+\\)" ghub-base-url)
+        (match-string 1 ghub-base-url)
+      (signal 'ghub-auth-error '("Invalid value for ghub-base-url")))))
 
 (defun ghub--username ()
   (or ghub-username
